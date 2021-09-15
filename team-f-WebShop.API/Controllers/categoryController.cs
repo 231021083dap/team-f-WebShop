37731ea9f31a11eb1272cb7eb9d17278f2c7aa1d
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-15 13:34:28 +0200

    Added categoryController

---
+++ team-f-WebShop.API/Controllers/categoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace team_f_WebShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class categoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
