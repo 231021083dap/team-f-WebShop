2c460e46c98adfb88aff017695b0048c7709372a
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-15 18:35:54 +0200

    categoryControllerTests testet

--- team-f-WebShop.Tests/categoryControllerTests.cs
+++ team-f-WebShop.Tests/categoryControllerTests.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using team_f_WebShop.API.Controllers;
+using Xunit;
 
 namespace team_f_WebShop.Tests
 {
-    class categoryControllerTests
+    public class categoryControllerTests
     {
+        [Fact]
+        public void GetAll_ShouldReturnStatusCode200_WhenDataExists()
+        {
+            // Arrange
+            categoryController controller = new categoryController();
+
+            // Act
+            var result = controller.GetAll();
+
+            // Assert
+            var statusCodeResult = (IStatusCodeActionResult)result;
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
     }
 }
