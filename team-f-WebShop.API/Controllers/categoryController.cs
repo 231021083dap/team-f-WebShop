4a577c7224c7cbc6fe6178b3ab1922172774a47f
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-16 09:39:33 +0200

    noget af dto

--- team-f-WebShop.API/Controllers/categoryController.cs
+++ team-f-WebShop.API/Controllers/categoryController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using team_f_WebShop.API.DTOs.Responses;
 
 namespace team_f_WebShop.API.Controllers
 {
@@ -14,7 +15,39 @@ namespace team_f_WebShop.API.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            return Ok("Hello World");
+            List<categoryResponse> categorys = new();
+
+            categorys.Add(new categoryResponse
+            {
+                Id = 1,
+                categoryName = "Computer"
+            });
+
+            categorys.Add(new categoryResponse
+            {
+                Id = 2,
+                categoryName = "Screen"
+            });
+
+            categorys.Add(new categoryResponse
+            {
+                Id = 3,
+                categoryName = "Webcam"
+            });
+
+            categorys.Add(new categoryResponse
+            {
+                Id = 4,
+                categoryName = "Printer"
+            });
+
+            categorys.Add(new categoryResponse
+            {
+                Id = 5,
+                categoryName = "Tablet"
+            });
+
+            return Ok(categorys);
         }
     }
 }
