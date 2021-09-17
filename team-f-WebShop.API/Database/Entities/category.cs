cac61def231b05eaf8a34777b924be158909482c
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-17 08:44:57 +0200

    database contex

---
+++ team-f-WebShop.API/Database/Entities/category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace team_f_WebShop.API.Database.Entities
+{
+    public class category
+    {
+    }
+}
---
+++ team-f-WebShop.API/Database/WebShopProjectContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace team_f_WebShop.API.Database
+{
+    public class WebShopProjectContext
+    {
+    }
+}
