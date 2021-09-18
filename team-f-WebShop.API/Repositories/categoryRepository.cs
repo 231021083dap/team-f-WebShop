f9c7f0114bc5955153df5423307ebeee8e8b6918
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-18 15:37:09 +0200

    tilføjet noget repository

---
+++ team-f-WebShop.API/Repositories/categoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using team_f_WebShop.API.Database;
+using team_f_WebShop.API.Database.Entities;
+
+namespace team_f_WebShop.API.Repositories
+{
+    public interface IcategoryRepository
+    {
+        IEnumerable<category> GetAll();
+    }
+    public class categoryRepository : IcategoryRepository
+    {
+        private readonly WebShopProjectContext _context;
+
+        public categoryRepository(WebShopProjectContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
