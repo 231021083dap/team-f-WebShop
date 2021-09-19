56878961e6a59d265b91f505b4252f4e4a2c9976
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-20 00:16:59 +0200

    inmemery

---
+++ team-f-WebShop.Tests/categoryRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using team_f_WebShop.API.Database;
+using team_f_WebShop.API.Repositories;
+
+namespace team_f_WebShop.Tests
+{
+    public class categoryRepositoryTests
+    {
+        private readonly categoryRepository _sut;
+        private readonly WebShopProjectContext _context;
+        private readonly DbContextOptions<WebShopProjectContext> options;
+
+    }
+}
