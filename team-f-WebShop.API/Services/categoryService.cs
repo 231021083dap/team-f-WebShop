3887c16e9713a09db51926952bb7d12827e549fb
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-20 21:02:29 +0200

    CRUD imp

--- team-f-WebShop.API/Services/categoryService.cs
+++ team-f-WebShop.API/Services/categoryService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using team_f_WebShop.API.Database.Entities;
+using team_f_WebShop.API.DTOs.Requests;
 using team_f_WebShop.API.DTOs.Responses;
 using team_f_WebShop.API.Repositories;
 
@@ -12,6 +13,11 @@ namespace team_f_WebShop.API.Services
     public interface IcategoryService
     {
         Task<List<categoryResponse>> GetAllcategory();
+        Task<categoryResponse> GetById(int categoryId);
+        Task<categoryResponse> Create(NewCategory newCategory);
+        Task<categoryResponse> Update(int categoryId, UpdateCategory updatecategory);
+        Task<bool> Delete(int categoryId);
+
     }
     public class categoryService : IcategoryService
     {
@@ -21,45 +27,60 @@ namespace team_f_WebShop.API.Services
         {
             _categoryRepository = categoryRepository;
         }
+
+        public async Task<categoryResponse> Create(NewCategory newCategory)
+        {
+            category category = new category
+            {
+                categoryName = newCategory.categoryName
+            };
+
+            category = await _categoryRepository.Create(category);
+
+            return category == null ? null : new categoryResponse
+            {
+                Id = category.Id,
+                categoryName = category.categoryName
+            };
+        }
+
+        public Task<bool> Delete(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<List<categoryResponse>> GetAllcategory()
         {
             List<category> categorys = await _categoryRepository.GetAll();
 
-            //categorys.Add(new categoryResponse
-            //{
-            //    Id = 1,
-            //    categoryName = "Computer"
-            //});
-
-            //categorys.Add(new categoryResponse
-            //{
-            //    Id = 2,
-            //    categoryName = "Screen"
-            //});
-
-            //categorys.Add(new categoryResponse
-            //{
-            //    Id = 3,
-            //    categoryName = "Webcam"
-            //});
-
-            //categorys.Add(new categoryResponse
-            //{
-            //    Id = 4,
-            //    categoryName = "Printer"
-            //});
-
-            //categorys.Add(new categoryResponse
-            //{
-            //    Id = 5,
-            //    categoryName = "Tablet"
-            //});
+            
 
             return categorys.Select(a => new categoryResponse
             {
                 Id = a.Id,
                 categoryName = a.categoryName
             }).ToList();
+        }
+
+        public Task<categoryResponse> GetById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<categoryResponse> Update(int categoryId, UpdateCategory updatecategory)
+        {
+            category category = new category
+            {
+                categoryName = updatecategory.categoryName
+            };
+
+            category = await _categoryRepository.Update(categoryId, category);
+
+            return category == null ? null : new categoryResponse
+            {
+                Id = category.Id,
+                categoryName = category.categoryName
+            };
         }
     }
 }
