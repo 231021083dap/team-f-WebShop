c66089b76dbc832ba35f76106fc085d1802614ff
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-20 08:48:37 +0200

    category Repository Testes

--- team-f-WebShop.Tests/categoryRepositoryTests.cs
+++ team-f-WebShop.Tests/categoryRepositoryTests.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using team_f_WebShop.API.Database;
+using team_f_WebShop.API.Database.Entities;
 using team_f_WebShop.API.Repositories;
+using Xunit;
 
 namespace team_f_WebShop.Tests
 {
@@ -13,7 +15,64 @@ namespace team_f_WebShop.Tests
     {
         private readonly categoryRepository _sut;
         private readonly WebShopProjectContext _context;
-        private readonly DbContextOptions<WebShopProjectContext> options;
+        private readonly DbContextOptions<WebShopProjectContext> _options;
+
+        public categoryRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<WebShopProjectContext>()
+                .UseInMemoryDatabase(databaseName: "WebShopProject")
+                .Options;
+
+            _context = new WebShopProjectContext(_options);
+
+            _sut = new categoryRepository(_context);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnListOfcategorys_WhencategroysExists() 
+        {
+            // Arrange
+            await _context.Database.EnsureDeletedAsync();
+            _context.category.Add(new category
+            {
+                Id = 1,
+                categoryName = "Computer"
+            });
+            _context.category.Add(new category
+            {
+                 Id = 2,
+                 categoryName = "Screen"
+            });
+            await _context.SaveChangesAsync();
+
+            // Act
+
+            var result = await _sut.GetAll();
+
+            // Assert
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.IsType<List<category>>(result);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnEmptyListOfcategorys_WhenNocategroysExists()
+        {
+            // Arrange
+            await _context.Database.EnsureDeletedAsync();
+
+            // Act
+
+            var result = await _sut.GetAll();
+
+            // Assert
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+            Assert.IsType<List<category>>(result);
+        }
+
 
     }
 }
