bc4fcbb529874b2995f2908a2acdbaa22ce6b388
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-21 11:31:23 +0200

    mere tests

--- team-f-WebShop.Tests/categoryServiceTeste.cs
+++ team-f-WebShop.Tests/categoryServiceTeste.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using team_f_WebShop.API.Database.Entities;
+using team_f_WebShop.API.DTOs.Requests;
 using team_f_WebShop.API.DTOs.Responses;
 using team_f_WebShop.API.Repositories;
 using team_f_WebShop.API.Services;
@@ -36,8 +37,8 @@ namespace team_f_WebShop.Tests
             });
             categorys.Add(new category
             {
-                     Id = 2,
-                     categoryName = "Screen"
+                 Id = 2,
+                 categoryName = "Screen"
             });
 
             _categoryRepository
@@ -53,6 +54,100 @@ namespace team_f_WebShop.Tests
             Assert.NotNull(result);
             Assert.Equal(2, result.Count);
             Assert.IsType<List<categoryResponse>>(result);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnEmptyListOfcategoryResponses_WhenNoCategoryExists()
+        {
+            // Arrange
+            List<category> categorys = new List<category>();
+
+            _categoryRepository
+                .Setup(a => a.GetAll())
+                .ReturnsAsync(categorys);
+
+            // Act
+            var result = await _sut.GetAllcategory();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Empty(result);
+            Assert.IsType<List<categoryResponse>>(result);
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnAncategoryResponse_WhencategoryExists()
+        {
+            // Arrange
+            int categoryId = 1;
+
+            category category = new category
+            {
+                Id = categoryId,
+                categoryName = "Computer"
+            };
+
+            _categoryRepository
+                .Setup(a => a.GetById(It.IsAny<int>()))
+                .ReturnsAsync(category);
+
+            // Act
+            var result = await _sut.GetById(categoryId);
+
+            // Asseret
+            Assert.NotNull(result);
+            Assert.IsType<categoryResponse>(result);
+            Assert.Equal(categoryId, result.Id);
+            Assert.Equal(category.categoryName, result.categoryName);
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnNull_WhencategoryDoesNotExist()
+        {
+            // Arrange
+            int categoryId = 1;
+
+            _categoryRepository
+                .Setup(a => a.GetById(It.IsAny<int>()))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _sut.GetById(categoryId);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void Create_ShouldReturncategoryResponse_WhenCreateIsSuccess()
+        {
+            // Arrange
+
+            NewCategory newCategory = new NewCategory
+            {
+                categoryName = "Computer"
+            };
+
+            int categoryId = 1;
+
+            category category = new category
+            {
+                Id = categoryId,
+                categoryName = "Computer"
+            };
+
+            _categoryRepository
+                .Setup(a => a.Create(It.IsAny<category>()))
+                .ReturnsAsync(category);
+
+            // Act
+            var result = await _sut.Create(newCategory);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<categoryResponse>(result);
+            Assert.Equal(categoryId, result.Id);
+            Assert.Equal(newCategory.categoryName, result.categoryName);
         }
 
     }
