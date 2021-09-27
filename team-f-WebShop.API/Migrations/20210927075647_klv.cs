44cf628655a0889b155e132199d81486d63476b5
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-27 10:38:19 +0200

    Start på angular

---
+++ team-f-WebShop.API/Migrations/20210927075647_klv.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace team_f_WebShop.API.Migrations
+{
+    public partial class klv : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
