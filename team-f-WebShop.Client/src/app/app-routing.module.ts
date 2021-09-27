7fcd86e56b356aa9fadb42a9e34a5321028924ec
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-27 18:27:40 +0200

    Staet på categoryside

--- team-f-WebShop.Client/src/app/app-routing.module.ts
+++ team-f-WebShop.Client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategorypageComponent } from './categorypage/categorypage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: CategorypageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
