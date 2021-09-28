8cc46ecad597811fd62c1d4de9fb3dabe6a4e20d
Klaus Jakobsen <mrklausj@gmail.com>
2021-09-28 09:17:33 +0200

    forsat med category page

--- team-f-WebShop.Client/src/app/category.service.ts
+++ team-f-WebShop.Client/src/app/category.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { category } from './models';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,4 +12,8 @@ export class CategoryService {
   private apiUrl = 'https://localhost:5001/api/category';
 
   constructor(private http:HttpClient) { }
+
+  getCategorys(): Observable<category[]>{
+    return this.http.get<category[]>(this.apiUrl);
+  }
 }
